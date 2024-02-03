8c815db51157d65dbb577507465f61a91afa33da
Rudolph919 <47603362+Rudolph919@users.noreply.github.com>
2024-02-03 13:15:24 +0200

    Reworked database interaction

--- config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'host' => 'mysql-db',           //container name of mysql in docker-compose.yaml
-    'dbname' => 'subscribers',      //database name defined in docker-compose.yaml
-    'username' => 'test_user',      //database user name defined in docker-compose.yaml
-    'password' => 'test_password',  //database user password defined in docker-compose.yaml
-    'charset' => 'utf8mb4',
-];
